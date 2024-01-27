6b7de0a04b126aebf392c5d04060f783429620d4
dandipeng <ddpeng@ucdavis.edu>
2024-01-26 17:35:13 -0800

    inital draft

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # you.explore
-personal website
+
+<img src="you time logo.png" alt="you.explore — 之莜" style="border-radius: 12px; border: 1px solid #e5e7eb" />
+
+<br>
+<br>
+
+My personal website is based on Onur Şuyalçınkaya's [git repo](https://github.com/suyalcinkaya/onur.dev). I'm deeply impressed by the 'E-mail' design concept and love its neatness and smoothness.
+
+## Overview
+
+- `/` - Home page.
+- `/[slug]` - Static pre-rendered pages using [Contentful](https://www.contentful.com). (e.g. `/stack`, `/about`)
+- `/writing` - Writing page.
+- `/writing/[slug]` - Static pre-rendered writing pages using [Contentful](https://www.contentful.com).
+- `/journey` - Journey page.
+- `/bookmarks` - Bookmarks page.
+- `/bookmarks/[id]` - Static pre-rendered bookmarks pages using [Raindrop](https://raindrop.io/).
+- `/api` - API routes.
+
+
+## Built With
+
+- [Next.js](https://nextjs.org)
+- [Tailwind CSS](https://tailwindcss.com)
+- [shadcn/ui](https://ui.shadcn.com)
+- [Contentful](https://www.contentful.com)
+- [Raindrop](https://raindrop.io)
+- [Supabase](https://supabase.com)
+- [Vercel](https://vercel.com)
---
+++ src/app/page.js
@@ -0,0 +1,47 @@
+import { Suspense } from 'react'
+import Link from 'next/link'
+
+import { ScrollArea } from '@/components/scroll-area'
+import { LoadingSpinner } from '@/components/loading-spinner'
+import { WritingList } from '@/components/writing-list'
+import { FloatingHeader } from '@/components/floating-header'
+import { PageTitle } from '@/components/page-title'
+import { Button } from '@/components/ui/button.jsx'
+import { getAllPosts } from '@/lib/contentful'
+import { getSortedPosts } from '@/lib/utils'
+
+async function fetchData() {
+  const allPosts = await getAllPosts()
+  return { allPosts }
+}
+
+export default async function Home() {
+  const { allPosts } = await fetchData()
+  const sortedPosts = getSortedPosts(allPosts)
+
+  return (
+    <ScrollArea className="flex flex-col" hasScrollTitle>
+      <FloatingHeader scrollTitle="Dandi 丹迪" />
+      <div className="content-wrapper">
+        <div className="content">
+          <PageTitle title="Home" className="lg:hidden" />
+          <p>
+            {`Hi 👋 I'm Dandi (丹迪 in Chinese), a data scientist, writer, and idealist based in LA,
+          USA.`}
+          </p>
+          <p>
+            I am a Data Scientist in work, reader, writer and listener in life. I'm interested in people, obsessed with experiencing different ones' lives in books and talkings.
+          </p>
+          <Button asChild variant="link" className="inline px-0">
+            <Link href="/writing">
+              <h2 className="mb-4 mt-8">Writing</h2>
+            </Link>
+          </Button>
+          <Suspense fallback={<LoadingSpinner />}>
+            <WritingList items={sortedPosts} header="Writing" />
+          </Suspense>
+        </div>
+      </div>
+    </ScrollArea>
+  )
+}
